054deabff99199afc62879533ab8164b8f3e0550
femi <oluwafemiogunleye21@gmail.com>
2022-09-24 05:59:03 -0700

    number of occurences in a text

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function noInputtedWord(word,text){
     ((word.trim().length === 0) || (text.trim().length === 0))
     return 0;
     }
+
     //function to counter total number of words in a text
 function wordCounter(text) {
     if (text.trim().length === 0) {
@@ -12,6 +13,21 @@ function wordCounter(text) {
     const wordArray = text.split(" ");
     wordArray.forEach(function(element) {
       if (!Number(element)) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+
+  // to check occurences of word in a text
+  function numberOfOccurrencesInText(word, text) {
+    if (word.trim().length === 0) {
+      return 0;
+    }
+    const wordArray = text.split(" ");
+    let wordCount = 0;
+    wordArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
         wordCount++;
       }
     });
