80424152564e71d67e07b46b1d753f7484746b07
femi <oluwafemiogunleye21@gmail.com>
2022-09-24 05:57:05 -0700

    add func to count words in a text

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@ function noInputtedWord(word,text){
     ((word.trim().length === 0) || (text.trim().length === 0))
     return 0;
     }
+    //function to counter total number of words in a text
+function wordCounter(text) {
+    if (text.trim().length === 0) {
+      
+    }
+    let wordCount = 0;
+    const wordArray = text.split(" ");
+    wordArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
   
